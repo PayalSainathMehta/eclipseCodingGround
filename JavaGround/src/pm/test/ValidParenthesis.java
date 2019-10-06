f5b590480979ba5f1852f7ea7136db186cb28ce7
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-10-06 01:38:13 -0400

    Valid Parenthesis in Java

---
+++ JavaGround/src/pm/test/ValidParenthesis.java
@@ -0,0 +1,36 @@
+package pm.test;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+    	HashMap<Character, Character>mapping = new HashMap<>();
+    	mapping.put(']', '[');
+    	mapping.put('}', '{');
+    	mapping.put(')', '(');
+    	
+    	Stack<Character>stack = new Stack<>();
+    	char ch,top;
+    	for(int i = 0; i < s.length(); i++) {
+    		 ch = s.charAt(i);
+    		if(mapping.containsKey(ch)) {
+    			top = stack.pop();
+    			if(top != mapping.get(ch))
+    				return false;
+       		}
+    		else {
+    			stack.push(ch);
+    		}
+    	}
+    	
+    	return stack.isEmpty();
+    }
+    
+    public static void main(String args[]) {
+    	ValidParenthesis obj = new ValidParenthesis();
+    	boolean res = obj.isValid("([])");
+    	System.out.println(res);
+    }
+
+}
