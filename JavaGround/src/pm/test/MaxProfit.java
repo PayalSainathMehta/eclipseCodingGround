bd2e7c6b60551f7fd485abb5d0ad0b19bc24f6f3
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-10-08 17:55:55 -0400

    Maximize profit of stock

---
+++ JavaGround/src/pm/test/MaxProfit.java
@@ -0,0 +1,29 @@
+package pm.test;
+
+public class MaxProfit {
+	public static void main(String[] args) {
+		int res = 0;
+		int[] prices = new int[] {7,1,5,3,6,4};
+		System.out.println(maximizeProfit(prices));
+		
+	}
+	
+	
+	public static int maximizeProfit(int[]prices) {
+		int minPrice = Integer.MAX_VALUE;
+		int maxProfit = 0;
+		for(int i = 0 ;i < prices.length; i++)
+		{
+			if (prices[i] < minPrice){
+				minPrice = prices[i];
+			}
+			else if(prices[i] - minPrice > maxProfit) {
+				maxProfit = prices[i] - minPrice;
+			}
+		}
+		
+		
+		return maxProfit;
+		
+	}
+}
