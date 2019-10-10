6b7ee1d51828232bff3700fde9af9d2a2b78528a
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-10-10 16:31:32 -0400

    Longest common prefix

---
+++ JavaGround/src/pm/test/LongestCommonPrefix.java
@@ -0,0 +1,46 @@
+package pm.test;
+
+import java.util.List;
+
+public class LongestCommonPrefix {
+	
+	
+	//return empty string if length of list passed is 0 or null strings are passed.
+	public static String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0) return "";
+        //else pass complete list to recursive function.
+        else return longestCommonPrefix(strs, 0, strs.length-1);
+        
+    }
+	
+	public static String longestCommonPrefix(String[]strs, int l, int r) {
+		//I am a recursive function. I check the left and right passed to me and if they are same, I return either of them.
+		if(l == r)
+			return strs[l];
+		//If they are not, i calculate the mid of the list 
+		int mid = (l+r)/2;
+		//I then recursively call myself on left and right parts.
+		String left = longestCommonPrefix(strs, l, mid);
+		String right = longestCommonPrefix(strs,mid+1,r);
+		//Finally i return the common prefix of my child nodes.
+		return commonPrefix(left,right);
+		
+	}
+	
+	public static String commonPrefix(String left, String right) {
+		//I find out the smallest of the strings passed to me and then traverse till that position in both strings and once i find something 
+		//that does not match, i stop and return upto that length.
+		int min = Math.min(left.length(), right.length());
+		for(int i = 0; i < min; i++)
+			if(left.charAt(i) != right.charAt(i))
+				return left.substring(0, i);
+	    return left.substring(0, min);
+
+	}
+	
+
+	public static void main(String[] args) {
+		String[] input = {"flower","flow","flight"};
+		System.out.println(longestCommonPrefix(input));
+	}
+}
