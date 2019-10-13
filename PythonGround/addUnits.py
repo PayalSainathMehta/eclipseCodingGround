fd8d317536e46cebfbbe67b44e923008c2971080
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-10-13 00:00:39 -0400

    Add individuals - complete

--- PythonGround/addUnits.py
+++ PythonGround/addUnits.py
@@ -4,17 +4,29 @@ def addUnits(s1,s2):
     len1 = len(s1)
     len2 = len(s2)
     if len1 < len2:
+        for i in range(len2-len1-1,-1,-1):
+            #print(i)
+            #print(s2[i])
+            res = res + s2[i]
         for i in range(len1):
             s = int(s1[i]) + int(s2[i])
             res = res + str(s)
-        for i in range(len2-len1-1,-1):
-            print(i)
-            print(s2[i])
-            res = res + s2[i]
+    elif len2 < len1:
+        for i in range(len1-len2-1,-1,-1):
+            #print(i)
+            #print(s2[i])
+            res = res + s1[i]
+        for i in range(len2):
+            s = int(s1[i]) + int(s2[i])
+            res = res + str(s)
+    else:
+        for i in range(len1):
+            s = int(s1[i]) + int(s2[i])
+            res = res + str(s)
     return res
 
 
 
-s1 = '9'
+s1 = '9999'
 s2 = '99'
 print(addUnits(s1,s2))
