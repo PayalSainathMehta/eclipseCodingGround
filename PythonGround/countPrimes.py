ec3996db448f550415deee916ad5918f262e6b43
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-10-18 23:25:51 -0400

    Counting Primes

--- PythonGround/countPrimes.py
+++ PythonGround/countPrimes.py
@@ -1,13 +1,17 @@
 import math
 def countPrimes(n):
    prime = [True] * n
+   print(prime)
    if n < 2:
        return 0
    else:
        prime[0] = prime[1] = False
+       print(prime)
        for i in range(2, int(math.sqrt(n)) + 1):
+           print("i:",i)
            if prime[i]:
                for j in range(i * i, n, i):
+                   print("j:",j)
                    prime[j] = False
        return sum(prime)
 
