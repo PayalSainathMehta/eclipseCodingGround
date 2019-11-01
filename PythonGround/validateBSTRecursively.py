990d62d84d2611dfad64008f3119717bc5f48a8d
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-11-01 17:24:04 -0400

    Validate BST recursively

---
+++ PythonGround/validateBSTRecursively.py
@@ -0,0 +1,83 @@
+# Definition for a binary tree node.
+import pdb
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def helper(node, lower = float('-inf'), upper = float('inf')):
+            if not node:
+                return True
+            value = node.val
+            if value <= lower or value >= upper:
+                return False
+            if not helper(node.left, lower,value):
+                return False
+            if not helper(node.right, value, upper):
+                return False
+            
+            return True
+        
+        return helper(root)    
+    
+    
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.__next__()
+            root = stringToTreeNode(line)
+            
+            ret = Solution().isValidBST(root)
+
+            out = (ret)
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
