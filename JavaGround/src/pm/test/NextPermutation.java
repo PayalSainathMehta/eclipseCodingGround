f7bc9a60fb858c17a5c15d7f03c8d8096c0820b5
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-11-09 19:00:34 -0500

    Next Permutation

---
+++ JavaGround/src/pm/test/NextPermutation.java
@@ -0,0 +1,16 @@
+package pm.test;
+
+import java.util.Arrays;
+
+public class NextPermutation {
+    public static void nextPermutation(int[] nums) {
+    	
+    }
+    
+    
+    public static void main(String args[]) {
+    	int nums[] = new int[]{3,2,1};
+    	NextPermutation.nextPermutation(nums);
+    	System.out.println(Arrays.toString(nums));
+    }
+}
