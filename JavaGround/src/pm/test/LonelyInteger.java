c0283f87a522f88dd216ed6bcef2bea76e103be5
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-11-19 07:41:43 -0500

    Find lonely integer

--- JavaGround/src/pm/test/LonelyInteger.java
+++ JavaGround/src/pm/test/LonelyInteger.java
@@ -3,13 +3,15 @@ package pm.test;
 public class LonelyInteger {
 	
 	public static int findLoner(int[] a) {
+		if(a.length % 2 == 0)
+			return -1;
 		int result = a[0];
 		for(int i = 1; i < a.length; i++)
 			result = result ^ a[i];
 		return result;
 	}
 	public static void main(String args[]) {
-		int []a = {1,2,1,2,4,4,3,5,5,3,6};
+		int []a = {1,2,1,2,4,4,3,5,5,3};
 	    System.out.println(findLoner(a));
 	}
 }
