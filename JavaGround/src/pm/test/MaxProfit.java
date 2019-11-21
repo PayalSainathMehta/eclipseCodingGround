f1e01976067b530cd5d89f6040132d808d7827d3
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-11-21 10:01:22 -0500

    Max Profit

--- JavaGround/src/pm/test/MaxProfit.java
+++ JavaGround/src/pm/test/MaxProfit.java
@@ -10,18 +10,23 @@ public class MaxProfit {
 	
 	
 	public static int maximizeProfit(int[]prices) {
-		int minPrice = Integer.MAX_VALUE;
-		int maxProfit = 0;
-		for(int i = 0 ;i < prices.length; i++)
-		{
-			if (prices[i] < minPrice){
-				minPrice = prices[i];
-			}
-			else if(prices[i] - minPrice > maxProfit) {
-				maxProfit = prices[i] - minPrice;
-			}
-		}
+//		int minPrice = Integer.MAX_VALUE;
+//		int maxProfit = 0;
+//		for(int i = 0 ;i < prices.length; i++)
+//		{
+//			if (prices[i] < minPrice){
+//				minPrice = prices[i];
+//			}
+//			else if(prices[i] - minPrice > maxProfit) {
+//				maxProfit = prices[i] - minPrice;
+//			}
+//		}
 		
+		int maxProfit = 0;
+		for(int i = 1; i < prices.length;i++) {
+			if(prices[i] > prices[i - 1])
+				maxProfit += prices[i] - prices[i - 1]; 
+		}
 		
 		return maxProfit;
 		
