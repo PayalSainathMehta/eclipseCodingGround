7c1c47c30e602af6c1278e8e156d983cf6e67148
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-11-25 12:33:05 -0500

    Path Sum

---
+++ JavaGround/src/pm/test/PathSum2.java
@@ -0,0 +1,120 @@
+package pm.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Paths {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> paths = new ArrayList<>();
+        findPaths(root, sum, new ArrayList<Integer>(),paths);
+        return paths;
+    }
+    
+    public void findPaths(TreeNode root, int sum, List<Integer>current, List<List<Integer>> paths){
+        if(root == null)
+            return;
+        current.add(root.val);
+        if(root.val == sum && root.left == null && root.right == null){
+            paths.add(current);
+            return;
+        }
+        findPaths(root.left, sum - root.val, new ArrayList<Integer>(current),paths);
+        findPaths(root.right, sum - root.val, new ArrayList<Integer>(current),paths);
+        
+    }
+}
+
+public class PathSum2 {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+    
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+    
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+    
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+    
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+    
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+    
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+            line = in.readLine();
+            int sum = Integer.parseInt(line);
+            
+            List<List<Integer>> ret = new Paths().pathSum(root, sum);
+            
+            String out = int2dListToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
