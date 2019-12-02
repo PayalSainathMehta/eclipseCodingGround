943990b94d046a3714ed6681b134bb721ffccede
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-12-01 23:48:47 -0500

    Burst balloons - minmum number of arrows

--- JavaGround/src/pm/test/BurstBalloons.java
+++ JavaGround/src/pm/test/BurstBalloons.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class BurstBalloons {
 	public static int countArrows(int points[][]) {
 		Arrays.sort(points,(a,b) -> a[1] - b[1]);
+		//System.out.println(Arrays.deepToString(points));
 		int pos = points[0][1];
+		//System.out.println(pos);
 		int count = 1;
 		for(int i = 1; i < points.length; i++)
 		{
@@ -13,13 +15,14 @@ public class BurstBalloons {
 				continue;
 			count = count + 1;
 			pos = points[i][1];
+			//System.out.println(pos);
 		}
 		
 		return count;
 	}
 	
 	public static  void main(String args[]) {
-		int[][] points = new int[][] {{1,6},{2,8},{7,12},{10,16}};
+		int[][] points = new int[][] {{2,8},{1,6},{7,12},{10,16}};
 		int result = BurstBalloons.countArrows(points);
 		System.out.println(result);
 	}
