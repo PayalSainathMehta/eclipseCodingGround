8594d0d1770fd71ab3ac37d9716e1e6d1fcda421
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-12-01 23:49:15 -0500

    Combination sum

--- JavaGround/src/pm/test/CombinationSum.java
+++ JavaGround/src/pm/test/CombinationSum.java
@@ -5,31 +5,29 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
+    public static List<List<Integer>> combinationSum(int[] nums, int target) {
     List<List<Integer>> list = new ArrayList<>();
     Arrays.sort(nums);
     backtrack(list, new ArrayList<>(), nums, target, 0);
     return list;
 }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
     if(remain < 0) return;
     else if(remain == 0) list.add(new ArrayList<>(tempList));   
     else{ 
-        //System.out.println("List"+list);
         for(int i = start; i < nums.length; i++){
             tempList.add(nums[i]);
             backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            // System.out.println("TempList"+tempList);
-            // System.out.println("List"+list);
             tempList.remove(tempList.size() - 1);
         }
     }
 } 
     public static void main(String args[])
     {
-    	CombinationSum obj = new CombinationSum();
-    	int a[] = {1,0,1,0};
-    	obj.combinationSum(a, 7);
-    }
+    	int a[] = {6,3,2,7};
+    	List<List<Integer>> list = new ArrayList<>();
+    	list = combinationSum(a, 7);
+    	System.out.println(list);
+     }
 }
