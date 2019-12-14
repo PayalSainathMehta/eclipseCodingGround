11f8e10f16be0ffdaf2d891a6724d484023454db
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-12-14 08:55:58 -0500

    LRU cache

--- JavaGround/src/pm/test/LRUCache.java
+++ JavaGround/src/pm/test/LRUCache.java
@@ -1,107 +1,94 @@
 package pm.test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class LRUCache {
-    
-    int maxCapacity;
-    int totalItemsInCache;
-    
-    Map<Integer,ListNode> hashTable = new HashMap<>();
-    ListNode head;
-    ListNode tail;
-    
+class LRUCache {
+	
+	int maxCapacity;
+	int totalItemsinCache;
+	
+	ListNode head = new ListNode();
+	ListNode tail = new ListNode();
+	
+	Map<Integer, ListNode> mp = new HashMap<>();
+	
+	private class ListNode{
+		int val;
+		int key;
+		ListNode prev;
+		ListNode next;
+	}
+	
 
-    public LRUCache(int capacity) {
-        totalItemsInCache = 0;
-        this.maxCapacity = capacity;
-            
-        head = new ListNode();
-        tail = new ListNode();
-        
-        head.next = tail;
-        tail.prev = head;
-        
-        
-    }
-    
-    public int get(int key) {
-        
-        ListNode node = hashTable.get(key);
-            
-        if (node == null) return -1;
-        
-        moveToHead(node);
-        
-        return node.value;
-        
-    }
-    
-    public void put(int key, int value) {
-        ListNode node = hashTable.get(key);
-            
-        if (node == null){
-            ListNode newNode = new ListNode();
-            newNode.key = key;
-            newNode.value = value;
-                
-            hashTable.put(key, newNode);
-            addToFront(newNode);
-            totalItemsInCache++;
-            
-            if (totalItemsInCache > maxCapacity)
-                removeLRUEntry();
-        }
-        else{
-            node.value = value;
-            moveToHead(node);
-        }
-    }   
-        private void moveToHead(ListNode node){
-            removeFromList(node);
-            addToFront(node);
-        }
-        
-        private void addToFront(ListNode node){
-            node.prev = head;
-            node.next = head.next;
-            head.next.prev = node;    
-            head.next = node;
-    }
-        private void removeFromList(ListNode node){
-            ListNode savedPrev = node.prev;
-            ListNode savedNext = node.next;
-            
-            savedPrev.next = savedNext;
-            savedNext.prev = savedPrev; 
-        }
-    
-        private void removeLRUEntry(){
-            ListNode tail = popTail();
-            hashTable.remove(tail.key);
-            --totalItemsInCache;
-        }
-    
-        private ListNode popTail(){
-            ListNode tailItem = tail.prev;
-            removeFromList(tailItem);
-            
-            return tailItem;
-        }
-  private class ListNode{
-    int key;
-    int value;
-    
-    ListNode prev;
-    ListNode next;
-}      
+	public LRUCache(int capacity) {
+		//initialize everything
+		totalItemsinCache = 0;
+		this.maxCapacity = capacity;
+		
+	
+		head.next = tail;
+		tail.prev = head;
+	}
+	
+	public int get(int key) {
+		ListNode node = mp.get(key);
+		if(node == null) return -1;
+		
+		moveToHead(node);
+		return node.val;
+	}
+	
+	
+	public void put(int key, int value) {
+		ListNode node = mp.get(key);
+		if(node == null) {
+			ListNode newNode = new ListNode();
+			newNode.key = key;
+			newNode.val = value;
+			
+			mp.put(key, newNode);
+			addToFront(newNode);
+			totalItemsinCache++;
+			
+			if(totalItemsinCache > maxCapacity)
+				removeLRUEntry();		
+		}
+		else {
+			node.val = value;
+			moveToHead(node);
+		}
+		
+	
+	}
+	
+	public void removeLRUEntry() {
+		ListNode tailItem = tail.prev;
+		removeFromList(tailItem);
+		mp.remove(tailItem);
+		--totalItemsinCache;
+	}
+	
+	public void removeFromList(ListNode node) {
+		ListNode savedPrev = node.prev;
+		ListNode savedNext = node.next;
+		
+		savedPrev.next = savedNext;
+		savedNext.prev = savedPrev;
+	}
+	
+	public void addToFront(ListNode node) {
+		node.prev = head;
+		node.next = head.next;
+		head.next.prev = node;
+		head.next = node;
+	}
+	
+	public void moveToHead(ListNode node) {
+		removeFromList(node);
+		addToFront(node);
+	}
+	
+	
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
-
