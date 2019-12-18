55d30f79f1a8b1ec25974779da015951d7e8ac90
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-12-18 07:24:03 -0500

    Maxprofit

--- JavaGround/src/pm/test/MaxProfit.java
+++ JavaGround/src/pm/test/MaxProfit.java
@@ -2,7 +2,6 @@ package pm.test;
 
 public class MaxProfit {
 	public static void main(String[] args) {
-		int res = 0;
 		int[] prices = new int[] {7,1,5,3,6,4};
 		System.out.println(maximizeProfit(prices));
 		
@@ -10,25 +9,24 @@ public class MaxProfit {
 	
 	
 	public static int maximizeProfit(int[]prices) {
-//		int minPrice = Integer.MAX_VALUE;
-//		int maxProfit = 0;
-//		for(int i = 0 ;i < prices.length; i++)
-//		{
-//			if (prices[i] < minPrice){
-//				minPrice = prices[i];
-//			}
-//			else if(prices[i] - minPrice > maxProfit) {
-//				maxProfit = prices[i] - minPrice;
-//			}
-//		}
-		
+		// with more than one transactions
 		int maxProfit = 0;
 		for(int i = 1; i < prices.length;i++) {
 			if(prices[i] > prices[i - 1])
 				maxProfit += prices[i] - prices[i - 1]; 
 		}
 		
-		return maxProfit;
+		//return maxProfit;
+		
+		
+		//with one transactions 
+		int maxcur = 0, maxsofar = 0;
+		for(int i = 1; i < prices.length; i++) {
+			maxcur = maxcur + prices[i] - prices[i - 1];
+			maxcur = Math.max(0, maxcur);
+			maxsofar = Math.max(maxcur, maxsofar);
+		}
+		return maxsofar;
 		
 	}
 }
