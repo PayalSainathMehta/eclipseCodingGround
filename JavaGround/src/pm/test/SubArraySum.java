4122bf05adf97c42cbe046bd06de70cf40a1c2d5
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-12-21 21:53:04 -0500

    Sub array sum

--- JavaGround/src/pm/test/SubArraySum.java
+++ JavaGround/src/pm/test/SubArraySum.java
@@ -1,21 +1,24 @@
 package pm.test;
 
 import java.util.HashMap;
+
 import java.util.Map;
 
 public class SubArraySum {
     public static int subarraySum(int[] nums, int k) {
     	int sum = 0, count = 0;
     	Map<Integer, Integer> map = new HashMap<>();
-    	map.put(0,1);
+    	//remain, count
+    	map.put(0,1); //when remain equals 0 then we add count as 1 
     	for(int i = 0; i < nums.length; i++) {
+    		//contiguous sum
     		sum = sum + nums[i];
+    		//check if the remaining is present in the map
     		if(map.containsKey(sum - k))
     			count = count + map.get(sum - k);
     		map.put(sum,map.getOrDefault(sum, 0) + 1);
-    	}
+    	} 
     	return count;
-    	
     }
     
     public static void main(String args[]) {
