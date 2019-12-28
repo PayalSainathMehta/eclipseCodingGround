a73a4d45c9b006b5d679c196e6f8a983ae1c17ff
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2019-12-27 23:59:32 -0500

    Circular petrol pump

--- JavaGround/src/pm/test/CircularListPetrolPump.java
+++ JavaGround/src/pm/test/CircularListPetrolPump.java
@@ -6,18 +6,21 @@ public class CircularListPetrolPump {
 		//length of array
 		int n = a.length;
 		//distance between first and second
+		//storing first difference
 		int current = a[0][0] - a[0][1];
+//		System.out.println(current);
 		int i = 0, j = 1;
 		while(i != j || current < 0) {
 			//first and second not same then check for distance
 			while(i != j && current < 0) {
-				current -= a[i][0] - a[i][1];
-				i = (i + 1) % n;
+				//definitely the first difference was negative so going inside for i = 0
+				current = current - (a[i][0] - a[i][1]);
+				i = (i + 1) % 
 				
 				if(i == 0)
 					return -1;
 			}
-		current += a[j][0] - a[j][1];
+		current = current + (a[j][0] - a[j][1]);
 		j = (j + 1) % n;
 	}		
 		return i;
