12441c4e97991ce9d48498d35ba9c2a7d493a6c1
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2020-01-02 08:50:03 +0530

    Largest Arithmetic Subsequence

--- JavaGround/src/pm/test/LargestArithmeticSubsequence.java
+++ JavaGround/src/pm/test/LargestArithmeticSubsequence.java
@@ -1,12 +1,27 @@
 package pm.test;
 
+import java.util.HashMap;
+
 public class LargestArithmeticSubsequence {
     public static int longestArithSeqLength(int[] A) {
-    	
+    	  int res = 2, n = A.length;
+          HashMap<Integer, Integer>[] dp = new HashMap[n];
+          //System.out.println(Arrays.toString(dp));
+             for (int i = 0; i < n; i++) {
+             dp[i] = new HashMap<>();
+             for (int j = 0; j < i; j++) {
+                 int d = A[i] - A[j];
+                 dp[i].put(d, dp[j].getOrDefault(d, 1) + 1);
+                 //System.out.println(dp);
+                 res = Math.max(res, dp[i].get(d));
+             }
+         }
+         return res;
     }
     
     public static void main(String args[]) {
-    	
+    	int a[] = {20,1,15,3,10,5,8};
+    	System.out.println(longestArithSeqLength(a));
     }
 
 }
